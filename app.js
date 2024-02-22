770217e291172cf196ad66672e8f9ace5632589b
BrayaCheat <123721732+BrayaCheat@users.noreply.github.com>
2024-02-22 17:10:27 +0700

    Change port 2

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ require('./src/utility/database')
 
 //Port Listen
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${PORT}`)
 })
